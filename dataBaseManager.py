b71a8e5a93e982e81d21f06efb0b4276276ce7f8
Israel_Winnett <arulik98@gmail.com>
2025-11-25 19:49:44 +0200

    update more classes

--- dataBaseManager.py
+++ dataBaseManager.py
@@ -1,5 +1,6 @@
 import sqlite3
-
+from person import Person
+from car import Car
 class DataBaseManager:
     def __init__(self, db_name = "persons_cars.db"):
         self.db_name = db_name
@@ -7,31 +8,65 @@ class DataBaseManager:
         self.cursor = self.connection.cursor()
 
     def creat_tables(self):
-        pass
+        create_table_person = self.cursor.execute("""CTREAT TABLE person(
+                                   
+                person_id INTEGER primary key
+                name TEXT NOT NULL
+                age  INTEGER NOT NULL
+                email TEXT UNQUE NOT NULL);                                                                           )
+            """)
+        create_table_car = self.cursor.execute("""CTREAT TABLE CAR(
+                                   
+                CAR_id INTEGER primary key
+                brand TEXT NOT NULL
+                model TEXT NOT NULL 
+                year INTEGER NOT NULL
+                color TEXT NOT NULL
+                owner_id INTEGER
+                FOREIGN KEY (owner_id) REFERENCES person(person_id)                                                         
+                                  );
+            """)
+        return create_table_person, create_table_car
 
     def insert_person(self):
-        pass
+        ins_person = self.cursor.execute(
+            "INSERT INTO person(" 
+            "VALUES)", "?,?,?,?"
+        )
+        return ins_person
 
     def insert_car(self):
-        pass
+        ins_car = self.cursor.execute(
+            "INSERT INTO car(" 
+            "VALUES)", "?,?,?,?"
+        )
+        return ins_car
+
 
     def get_all_persons(self):
-        pass
+        select_person = self.cursor.execute("select * from person")
+        return select_person
 
     def get_all_cars(self):
-        pass
+        select_car = self.cursor.execute("select * from car")
+        return select_car
+
 
     def get_person_by_id(self, person_id):
-        pass
+        get_person_id = self.cursor.execute(f"SELECT * FORM person WHERE {person_id}")
+        return get_person_id
 
     def get_car_by_owner(self, owner_id):
-        pass
+        get_car_id = self.cursor.execute(f"SELECT * FORM car WHERE {owner_id}")
+        return get_car_id
 
-    def update_person(self, person):
-        pass
+    def update_person(self, person, result):
+        update = self.cursor.execute(f"UPDATE person SET {person} = {result}")
+        return update
 
     def delete_person(self, person_id):
-        pass
+        delete = self.cursor.execute(f"DELETE FROM person WHERE {person_id}")
+        return "The person deleted succsfully!"
 
     def close(self):
         self.connection.close()
